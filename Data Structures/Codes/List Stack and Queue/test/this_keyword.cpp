1fc0c2ff3d99788aa40baa5a5e8a9c7651ec72ba
isteakahamedimon <217084758+isteakahamedimon@users.noreply.github.com>
2026-08-12 16:25:12 +0600

    This version will result in Compilation Error. Compilaiton happens before a program runs/executes. Compilation is a top-to-bottom process. This code will throw Compilation Error because Identity* comes before the declaration of Identity class.

--- Data Structures/Codes/List Stack and Queue/test/this_keyword.cpp
+++ Data Structures/Codes/List Stack and Queue/test/this_keyword.cpp
@@ -1,5 +1,10 @@
 #include<iostream>
 
+void PrintIdentity(Identity* I)
+{
+    std::cout << I->name << " " << I->age << "\n";
+}
+
 class Identity
 {
     private:
@@ -9,24 +14,20 @@ class Identity
         int age;
 
         Identity(std::string name, int age)
-            : name(name), age(age) {
+        {
              // this is a pointer and it holds the address of the object 
-               std::cout << this << std::endl;
-            }
+             this->name = name;
+             this->age = age;
 
-        void showIdentity() {
-            std::cout << "Name: " << this->name << " Age: " << this->age << std::endl;
-
-            std::cout << this << std::endl;
-
+             PrintIdentity(this);
         }
 
+        std::cout << *this << std::endl;       
 };
 
 int main()
 {
     Identity me("Isteak", 23);
-    me.showIdentity();
 
     return 0;
 }
