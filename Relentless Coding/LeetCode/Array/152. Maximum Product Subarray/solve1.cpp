70a7a4790478563edc6dd0cc8b938c50e791803e
isteakahamedimon <217084758+isteakahamedimon@users.noreply.github.com>
2026-08-29 18:54:30 +0600

    This get accepted in Leetcode

--- Relentless Coding/LeetCode/Array/152. Maximum Product Subarray/solve1.cpp
+++ Relentless Coding/LeetCode/Array/152. Maximum Product Subarray/solve1.cpp
@@ -1,26 +1,29 @@
 #include<iostream>
 
 int maxProduct(std::vector<int>& nums) {
-   int ans = INT_MIN;
-   int mul = 1;
-   int subMul = 1;
-   bool division = true;
+    int ans = INT_MIN;
+    int mul = 1;
+    int subMul = 1;
+    bool division = true;
 
-   for(size_t i=0; i<nums.size(); i++) {
-       
-       mul *= nums[i];
-       subMul *= nums[i];
+    for(size_t i=0; i<nums.size(); i++) {
+        
+        mul *= nums[i];
+        subMul *= nums[i];
 
-       ans = std::max(ans , std::max(mul, subMul));
+        ans = std::max(ans , std::max(mul, subMul));
 
-       if(nums[i] < 0 && division) {
-           subMul = 1;
-           division = false;
-       }
-       if(nums[i] == 0) subMul = 1;
-   }
+        if(nums[i] < 0 && division) {
+            subMul = 1;
+            division = false;
+        }
+        if(nums[i] == 0) {
+            mul = subMul = 1;
+            division = true;
+        }
+    }
 
-   return ans;
+    return ans;
 }
 
 int main()
